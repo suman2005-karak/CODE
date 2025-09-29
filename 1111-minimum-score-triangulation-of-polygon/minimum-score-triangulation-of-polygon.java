6b7fd9c894d73df86c9fd06755829ed7fe797f84
suman karak <sumankarak43@gmail.com>
2025-09-29 22:01:25 +0530

    Time: 5 ms (38.08%) | Memory: 41.7 MB (17.96%) - LeetSync

---
+++ 1111-minimum-score-triangulation-of-polygon/minimum-score-triangulation-of-polygon.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int minScoreTriangulation(int[] values) {
+        int n = values.length;
+        int[][] dp = new int[n][n];
+
+        // dp[i][j] = minimum score to triangulate polygon between i and j
+        for (int len = 2; len < n; len++) {
+            for (int i = 0; i + len < n; i++) {
+                int j = i + len;
+                dp[i][j] = Integer.MAX_VALUE;
+                for (int k = i + 1; k < j; k++) {
+                    dp[i][j] = Math.min(dp[i][j],
+                        dp[i][k] + dp[k][j] + values[i] * values[k] * values[j]);
+                }
+            }
+        }
+        return dp[0][n - 1];
+    }
+}
