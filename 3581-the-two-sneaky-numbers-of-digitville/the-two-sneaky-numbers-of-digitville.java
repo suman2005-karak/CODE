903814f01f8b1c5b6d74b180fce0bba60cfff5c9
suman karak <sumankarak43@gmail.com>
2025-10-31 11:06:22 +0530

    Time: 3 ms (36.86%) | Memory: 44.9 MB (27.53%) - LeetSync

---
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int[] getSneakyNumbers(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        List<Integer> result = new ArrayList<>();
+        
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                result.add(num);
+            } else {
+                seen.add(num);
+            }
+        }
+        
+        return new int[]{result.get(0), result.get(1)};
+    }
+}
