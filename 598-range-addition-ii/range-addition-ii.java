59ba5f48d12c9e092733f000630506011f48456b
suman karak <sumankarak43@gmail.com>
2025-11-08 09:53:12 +0530

    Time: 0 ms (100.00%) | Memory: 48.7 MB (5.80%) - LeetSync

---
+++ 598-range-addition-ii/range-addition-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxCount(int m, int n, int[][] ops) {
+        // If no operations, all values are same, return total cells
+        if (ops.length == 0) return m * n;
+        
+        int minRow = m;
+        int minCol = n;
+        
+        for (int[] op : ops) {
+            minRow = Math.min(minRow, op[0]);
+            minCol = Math.min(minCol, op[1]);
+        }
+        
+        return minRow * minCol;
+    }
+}
