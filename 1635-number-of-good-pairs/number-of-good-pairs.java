917622f6f58861bb8f6731bf00c40352e28978dc
suman karak <sumankarak43@gmail.com>
2025-11-11 09:32:58 +0530

    Time: 0 ms (100.00%) | Memory: 42.9 MB (6.65%) - LeetSync

---
+++ 1635-number-of-good-pairs/number-of-good-pairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int[] count = new int[101]; // Since 1 <= nums[i] <= 100
+        int goodPairs = 0;
+
+        for (int num : nums) {
+            // Each previous occurrence of num can form a good pair with current num
+            goodPairs += count[num];
+            count[num]++;
+        }
+
+        return goodPairs;
+    }
+}
