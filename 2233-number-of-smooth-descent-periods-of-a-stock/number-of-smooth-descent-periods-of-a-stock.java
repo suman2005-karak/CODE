066c7756905d39241c466e813ae466ad21858437
suman karak <sumankarak43@gmail.com>
2025-12-15 10:54:06 +0530

    Time: 2 ms (100.00%) | Memory: 83.8 MB (68.15%) - LeetSync

---
+++ 2233-number-of-smooth-descent-periods-of-a-stock/number-of-smooth-descent-periods-of-a-stock.java
@@ -0,0 +1,17 @@
+class Solution {
+    public long getDescentPeriods(int[] prices) {
+        long ans = 1;   // first day
+        long len = 1;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] == prices[i - 1] - 1) {
+                len++;
+            } else {
+                len = 1;
+            }
+            ans += len;
+        }
+
+        return ans;
+    }
+}
